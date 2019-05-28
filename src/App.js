f74ee539239633ed50b634a60e53404bdcb7a372
Nicolás Gómez <nigomezgu@unal.edu.co>
2019-05-28 13:28:44 -0500

    Primer grid para App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-
-const useStyles 
+import Grid from '@material-ui/core/Grid'
+import { Container } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hola.
-      </header>
-    </div>
+    <Grid className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" />
+      
+
+    </Grid>
   );
 }
 
