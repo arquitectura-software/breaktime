5247b4e9e96570cba174e9d902b86ac9054eb204
Nicolás Gómez <nigomezgu@unal.edu.co>
2019-07-07 01:20:16 -0500

    Arreglos Autenticación Admins y LocalStorage

--- src/components/auth.js
+++ src/components/auth.js
@@ -10,25 +10,38 @@ class Auth {
     }
 
     loginAdmin(cb){
+        window.localStorage.removeItem("token");
         this.authenticatedAdmin = true;
         cb();
     }
 
     logout(cb){
+        window.localStorage.removeItem("token");
+        window.localStorage.removeItem("idUser");
+        window.localStorage.removeItem("user");
         this.authenticated = false;
         cb();
     }
 
     logoutAdmin(cb){
+        window.localStorage.removeItem("token");
+        window.localStorage.removeItem("user");
         this.authenticatedAdmin = false;
         cb();
     }
 
     isAuthenticated(){
+        console.log(window.localStorage.getItem("token"))
+        if(window.localStorage.getItem("token").length > 1){ //TODO ISVALID
+            return true;
+        } 
         return this.authenticated;
     }
 
     isAuthenticatedAdmin(){
+        if(window.localStorage.getItem("token").length > 1){ //TODO ISVALID
+            return true;
+        } 
         return this.authenticatedAdmin;
     }
 }
