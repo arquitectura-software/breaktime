70d007125c88050876090816e5bc2ec186a39ae1
Nicolás Gómez <nigomezgu@unal.edu.co>
2019-07-15 23:40:25 -0500

    Comentando linea no funcional

--- src/components/protectedRouteAdmin.js
+++ src/components/protectedRouteAdmin.js
@@ -6,7 +6,7 @@ export const ProtectedRouteAdmin = ({component: Component, ...rest}) => {
     return(
         <Route {...rest} 
             render={(props) => {
-                auth.checkToken();
+                //auth.checkToken(); //Revisar si la petición es la misma.
                 if(auth.isAuthenticatedAdmin()){
                     return <Component {...props}/>
                 }else{
