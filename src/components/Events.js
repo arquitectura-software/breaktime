88ca25ed6a91fb438e2afbce1fc179afc5098b97
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2019-07-18 20:02:06 -0500

    fix filters-events

--- src/components/Events.js
+++ src/components/Events.js
@@ -66,17 +66,28 @@ class Events extends Component{
         familia: false,
       },
       tipo: {
+        diversión: false,
         beneficencia: false,
-        teatros: false,
-        bailes: false,
-        pilates: false,
-        zumba: false,
-        yoga: false,
+        relajación: false,
+        acondicionamiento: false,
+        entretenimiento: false
       },
       
     }
 
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  async decodificarDatos(){
+    let decodedCards =[]
+    for(let i=0; i< this.state.cards.length;i++){
+      let auxCard = this.state.cards[i]
+      for(let j in auxCard){
+        auxCard.j= wtf8.decode(auxCard[j]);
+      }
+      decodedCards.push(auxCard);
+    }
+    await this.setState({ cards: decodedCards})
   }
 
   componentDidMount(){
@@ -106,7 +117,14 @@ class Events extends Component{
           data[i]["button1"] = button1;
           data[i]["button2"] = button2;
           data[i].date = fecha;
+          switch(data[i].audence){
+            case "Para toda la familia": data[i].audence = "familia";
+            break;
+            default:
 
+                data[i].audence = wtf8.decode(data[i].audence).toLowerCase();
+          }
+          data[i].tipo = wtf8.decode(data[i].tipo).toLowerCase();
           this.setState({
             cards: this.state.cards.concat(data[i])
           })
@@ -119,21 +137,23 @@ class Events extends Component{
     let name = event.target.value;
     let publico = this.state.publico
     let tipo = this.state.tipo
-
+    console.log("tipo: ",tipo)
     if (!(this.state.publico[name] === undefined)) {
+      
       let pb = this.state.publico;
       console.log("not undifined")
       pb[name] = !this.state.publico[name];
       await this.setState({ [publico]: pb });
     }
     else {
+      console.log("en tipo")
       let ty = this.state.tipo;
+      console.log("this is ty:",ty,"name: ",name); 
       ty[name] = !this.state.tipo[name];
+      console.log("lo que actualiza: ",name,": ",ty[name]);
       await this.setState({ [tipo]: ty });
     }
-    //await this.setState({ ...this.state, [name]: event.target.checked });
-    //let {niños, adultos, adultosm, conciertos, teatros, bailes, pilates, zumba, yoga} = this.state;
-    //console.log(niños, adultos, adultosm, conciertos, teatros, bailes, pilates, zumba, yoga)
+    
   };
   render(){
 
@@ -147,13 +167,13 @@ class Events extends Component{
     publicoB.push(publicoC.adultos);
     publicoB.push(publicoC.familia);
 
-    
+    console.log("en el render: ",this.state.tipo.diversión)
     tipoB.push(tipoC.beneficencia);
-    tipoB.push(tipoC.teatros);
-    tipoB.push(tipoC.bailes);
-    tipoB.push(tipoC.pilates);
-    tipoB.push(tipoC.zumba);
-    tipoB.push(tipoC.yoga);
+    tipoB.push(tipoC.diversión);
+    tipoB.push(tipoC.relajación);
+    tipoB.push(tipoC.acondicionamiento);
+    tipoB.push(tipoC.entretenimiento);
+    console.log("TIPOB: ",tipoB)
 
     let card2 = this.state.cards.map(card => {
 
@@ -178,30 +198,36 @@ class Events extends Component{
             <Card key={card.id} card={card} />
           )
         }
+        //console.log("state.tipo[card]: ",card.audence," ",this.state.publico[card.audence]);
+        
+
         if (this.state.publico[card.audence] === true) {
           console.log("Entra al menos aqui");
           return (
-            <Card key={card.id} card={card} />
+            <Card key={card.id} card={card}/>
           )
         }
 
       }
       else if (tipoB.includes(true)) {
         console.log("Solo tipo");
-        if (!this.state.tipo.diversion && !this.state.tipo.beneficencia && !this.state.tipo.relajacion && !this.state.tipo.acondicionamiento && !this.state.tipo.entretenimiento) {
+        if (!this.state.tipo.diversión && !this.state.tipo.beneficencia && !this.state.tipo.relajación && !this.state.tipo.acondicionamiento && !this.state.tipo.entretenimiento) {
           return (
             <Card key={card.id} card={card} />
           )
         }
+        console.log("state.tipo[card]: ",card.tipo," ",this.state.tipo[card.tipo]);
         if (this.state.tipo[card.tipo] === true) {
           return (
             <Card key={card.id} card={card} />
           )
         }
+      }else{
+        return (
+          <Card key={card.id} card={card} /> //Check for bugs (Cambio warning).
+        )
       }
-      return (
-        <Card key={card.id} card={card} /> //Check for bugs (Cambio warning).
-      )
+      
 
     });
 
@@ -214,70 +240,54 @@ class Events extends Component{
               <Container maxWidth="lg" direction="row" className={classes.container}>
                 <Grid container direction="row" justify="flex-start" alignItems="flex-start">
                   <Grid item xs={12} sm={4} md={3}><Container>
-
-
                   <Grid container direction="column">
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend" className={classes.labelPublico}>Tipo de público</FormLabel>
-        <Divider></Divider>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox checked={this.state.publico.niños} onChange={this.handleChange} value="niños" />}
-            label="Niños"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={this.state.publico.adultos} onChange={this.handleChange} value="adultos" />}
-            label="Adultos"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={this.state.publico.adultosm} onChange={this.handleChange} value="adultosm" />
-            }
-            label="Adultos mayores"
-          />
-        </FormGroup>
-      </FormControl>
+                    <FormControl component="fieldset" className={classes.formControl}>
+                      <FormLabel component="legend" className={classes.labelPublico}>Tipo de público</FormLabel>
+                      <Divider></Divider>
+                      <FormGroup>
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.publico.niños} onChange={this.handleChange} value="niños" />}
+                          label="Niños"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.publico.adultos} onChange={this.handleChange} value="adultos" />}
+                          label="Adultos"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.publico.adultosm} onChange={this.handleChange} value="familia" />}
+                          label="Familia"
+                        />
+                      </FormGroup>
+                    </FormControl>
 
-      <FormControl component="fieldset" className={classes.formControl}>
-        <Divider></Divider>
-        <FormLabel component="legend" className={classes.labelPublico}>Tipo de evento</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox checked={this.state.tipo.beneficencia} onChange={this.handleChange} value="beneficencia" />}
-            label="Beneficiencia"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={this.state.tipo.teatros} onChange={this.handleChange} value="teatros" />}
-            label="Teatros"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={this.state.tipo.bailes} onChange={this.handleChange} value="bailes" />
-            }
-            label="Bailes"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={this.state.tipo.pilates} onChange={this.handleChange} value="pilates" />}
-            label="Pilates"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={this.state.tipo.umba} onChange={this.handleChange} value="zumba" />}
-            label="Zumba"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={this.state.tipo.yoga} onChange={this.handleChange} value="yoga" />
-            }
-            label="Yoga"
-          />
-        </FormGroup>
-        <FormHelperText>¡Disfrute de su estadía!</FormHelperText>
-      </FormControl>
-    </Grid>
-
-
-
-
+                    <FormControl component="fieldset" className={classes.formControl}>
+                      <Divider></Divider>
+                      <FormLabel component="legend" className={classes.labelPublico}>Tipo de evento</FormLabel>
+                      <FormGroup>
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.tipo.diversión} onChange={this.handleChange} value="diversión" />}
+                          label="Diversión"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.tipo.beneficencia} onChange={this.handleChange} value="beneficencia" />}
+                          label="Beneficencia"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.tipo.relajacion} onChange={this.handleChange} value="relajación" />}
+                          label="Relajación"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.entretenimiento} onChange={this.handleChange} value="entretenimiento" />}
+                          label="Entretenimiento"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={this.state.acondicionamiento} onChange={this.handleChange} value="acondicionamiento" />}
+                          label="Acondicionamiento"
+                        />
+                      </FormGroup>
+                      <FormHelperText>¡Disfrute de su estadía!</FormHelperText>
+                    </FormControl>
+                  </Grid>
                   </Container></Grid>
                   <Grid container xs={12} sm={8} md={9} item={true} spacing={2}>
                     {card2}
